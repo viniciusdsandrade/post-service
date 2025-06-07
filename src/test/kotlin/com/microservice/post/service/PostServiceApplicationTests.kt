0cf4a68c769da39263364504985ebfa52bda0fe4
Vinícius Andrade <vinicius_andrade2010@hotmail.com>
2025-06-07 16:07:29 -0300

    feat: add .gitignore to exclude IDE configuration files

---
+++ src/test/kotlin/com/microservice/post/service/PostServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.microservice.post.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
